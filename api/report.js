fe7ddb535707c62287b9ac3f3fe6943f6c1ffb2e
Underdog-000 <ilonmussk20@gmail.com>
2025-04-15 14:47:26 +0300

    Update report.js

--- api/report.js
+++ api/report.js
@@ -1,6 +1,6 @@
 // report.js
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 
 if (req.method !== 'POST') return res.status(405).send('Only POST allowed');
 
